aa1dbd5e11617f310e406d77e6ae025343489982
ziyodullayevj7 <ziyodullayevj7@gmail.com>
2026-08-08 15:50:44 +0500

    First commit

---
+++ src/main/java/teamwork/TozaCoin/TozaCoinApplication.java
@@ -0,0 +1,13 @@
+package teamwork.TozaCoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TozaCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TozaCoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamwork/TozaCoin/TozaCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package teamwork.TozaCoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TozaCoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
