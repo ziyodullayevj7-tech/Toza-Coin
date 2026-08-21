d63d737dbc3452b54fb3e74ae57ece4007b73952
ziyodullayevj7 <ziyodullayevj7@gmail.com>
2026-08-21 17:42:13 +0500

    entity edited

--- src/main/java/teamwork/repository/ProfileRepository.java
+++ src/main/java/teamwork/repository/ProfileRepository.java
@@ -2,8 +2,13 @@ package teamwork.repository;
 
 import org.hibernate.internal.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import teamwork.entity.ProfileEntity;
 
 public interface ProfileRepository extends JpaRepository<ProfileEntity, String> {
-    Optional<ProfileEntity> findByUsername(String username);
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN TRUE ELSE FALSE END " +
+            "FROM ProfileEntity AS p " +
+            "WHERE p.username = :username")
+    boolean existsByUsername(@Param("username") String username);
 }
